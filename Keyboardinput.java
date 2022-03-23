b9f6861abae300359e9d9763e3a8f317abaab9ec
Leonardo Adler da Silva <88751032+LeoAdlerr@users.noreply.github.com>
2022-03-23 01:45:13 -0300

    Create Keyboardinput.java

---
+++ Keyboardinput.java
@@ -0,0 +1,15 @@
+
+	public static void main(String[] args) 
+	throws java.io.IOException {
+		
+		char input;
+		
+		System.out.println("Enter a single character from the keyboard, the press ENTER: ");	
+		
+		input = (char) System.in.read();
+		
+		if(input == '1') System.out.println("You typed the number 1! ");
+		if(input != '1') System.out.println("You did not type the number 1!");
+		
+	}
+
